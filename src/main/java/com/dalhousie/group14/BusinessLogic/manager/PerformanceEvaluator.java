30e3d97146e4689fb364bd46f88eea51de07a361
Harjot Singh <harjot.sidhu33@gmail.com>
2021-07-09 11:03:03 +0530

    Code Formatting Corrected.

--- src/main/java/com/dalhousie/group14/BusinessLogic/manager/PerformanceEvaluator.java
+++ src/main/java/com/dalhousie/group14/BusinessLogic/manager/PerformanceEvaluator.java
@@ -1,6 +1,7 @@
 package com.dalhousie.group14.BusinessLogic.manager;
 
 import com.dalhousie.group14.Database.manager.PerformanceEvaluatorDB;
+
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -8,14 +9,14 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
-public class PerformanceEvaluator implements EvaluatorProperties{
+public class PerformanceEvaluator implements EvaluatorProperties {
     Map<String, ArrayList<String>> employeesessions = new HashMap<>();
-    Map<String, Integer> emp_discipline_rating = new HashMap<>();
+    Map<String, Float> emp_discipline_rating = new HashMap<>();
 
-    public void evaluateAll(){
+    public void evaluateAll() {
         ResultSet resultset = PerformanceEvaluatorDB.getEmployeeSessions();
-        if(resultset!=null){
-            while (true){
+        if (resultset != null) {
+            while (true) {
                 try {
                     if (!resultset.next()) {
                         break;
@@ -31,41 +32,40 @@ public class PerformanceEvaluator implements EvaluatorProperties{
 
     }
 
-    private void sessionmanager(String username, String minutes){
-        if(!employeesessions.containsKey(username)){
+    private void sessionmanager(String username, String minutes) {
+        if (!employeesessions.containsKey(username)) {
             ArrayList dailysession = new ArrayList();
             dailysession.add(minutes);
-            employeesessions.put(username,dailysession);
-        }else{
+            employeesessions.put(username, dailysession);
+        } else {
             ArrayList<String> dailysession = employeesessions.get(username);
             dailysession.add(minutes);
-            employeesessions.put(username,dailysession);
+            employeesessions.put(username, dailysession);
         }
 
     }
 
-    private void sessionanalysis(){
+    private void sessionanalysis() {
         Set<String> keyset = employeesessions.keySet();
-        for(String s : keyset){
+        for (String s : keyset) {
             ArrayList<String> monthsessions = employeesessions.get(s);
             int cummulitative_rating = 0;
-            for(int i  = 0 ; i < monthsessions.size() ; i++ ){
+            for (int i = 0; i < monthsessions.size(); i++) {
                 String minutes = monthsessions.get(i);
                 int day_rating = sessionrate(minutes);
                 cummulitative_rating = cummulitative_rating + day_rating;
             }
-            int employee_rating = (cummulitative_rating/days) * 100;
-
-
+            float employee_rating = (cummulitative_rating / days) * 100;
+            emp_discipline_rating.put(s, employee_rating);
         }
 
     }
 
-    private int sessionrate(String minutes){
+    private int sessionrate(String minutes) {
         int session_minutes = Integer.parseInt(minutes);
         int difference = maximumsession - session_minutes;
-        int rating_differential = difference%10;
-        int finalrating = maxrating - rating_differential  + 1;
+        int rating_differential = difference % 10;
+        int finalrating = maxrating - rating_differential + 1;
         return finalrating;
     }
 }
