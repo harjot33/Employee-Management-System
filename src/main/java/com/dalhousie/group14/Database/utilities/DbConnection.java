d46611914c3a055dd29992f41320d4a3ad9cc1bb
Jainam Rakeshkumar Shah <jn326778@dal.ca>
2021-07-09 18:23:26 +0530

    setup DbConnection.java class.

--- src/main/java/com/dalhousie/group14/Database/utilities/DbConnection.java
+++ src/main/java/com/dalhousie/group14/Database/utilities/DbConnection.java
@@ -1,16 +1,17 @@
 package com.dalhousie.group14.Database.utilities;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DbConnection {
 
-        public static void connectDB() throws SQLException {
-        Connection connection;
+    public static Connection connection;
+    public static void connectDB() throws SQLException {
         final String url = "jdbc:mysql://34.134.143.1/ems";
         final String username = "root";
         final String password = "ahjnr5";
         connection = DriverManager.getConnection(url, username, password);
-        System.out.println("successfully connection");
+
     }
 }
