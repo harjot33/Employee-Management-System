c4f1228e311b83b49b5294c68cdf35a7cc26bdb4
Jainam Rakeshkumar Shah <jn326778@dal.ca>
2021-07-09 17:25:05 +0530

    Initially setup login page.

---
+++ src/main/java/com/dalhousie/group14/Presentation/utilities/Login.java
@@ -0,0 +1,30 @@
+package com.dalhousie.group14.Presentation.utilities;
+
+import java.util.Scanner;
+import static com.dalhousie.group14.BusinessLogic.utilities.Login.loginEmployee;
+
+public class Login {
+
+    public void login() {
+        System.out.println("................WELCOME TO LOGIN PAGE...............");
+        Scanner scanner = new Scanner(System.in);
+        int select;
+        try {
+            do {
+                System.out.println("Press 1 for Employee Login: \n Press 2 for Manager Login: \n Press 3 for Client Login:");
+                select = scanner.nextInt();
+                if (select == 1) {
+                    loginEmployee();
+                } else if (select == 2) {
+                    //loginManager();
+                } else if (select == 3) {
+                    //loginClient();
+                } else {
+                    System.out.println("Please Enter correct number:");
+                }
+            } while (true);
+        } catch (Exception e) {
+            System.out.println("Exception:"+e);
+        }
+    }
+}
