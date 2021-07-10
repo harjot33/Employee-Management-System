a88da36a51a65961ee740de6b017805d803b7891
Jainam Rakeshkumar Shah <jn326778@dal.ca>
2021-07-10 23:37:12 +0530

    Update UpdateSecurityQuestion.java  connection object.

--- src/main/java/com/dalhousie/group14/Database/employee/UpdateSecurityQuestion.java
+++ src/main/java/com/dalhousie/group14/Database/employee/UpdateSecurityQuestion.java
@@ -1,10 +1,13 @@
 package com.dalhousie.group14.Database.employee;
 
+import com.dalhousie.group14.Database.utilities.DbConnection;
+
+import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 import static com.dalhousie.group14.Database.utilities.DbConnection.connectDB;
-import static com.dalhousie.group14.Database.utilities.DbConnection.connection;
+
 
 public class UpdateSecurityQuestion {
     public static void updateSecurityQuestion(String username, String question1, String question2, String question3) {
@@ -14,7 +17,7 @@ public class UpdateSecurityQuestion {
 
         String query = "UPDATE `ems`.`securityquestion` SET `securityQuestion1` = '" + question1 + "', `securityQuestion2` = '" + question2 + "', `securityQuestion3` = '" + question3 + "' WHERE (`UserName` = '" + username + "')";
         try{
-            connectDB();
+            Connection connection= DbConnection.connectDB();
             statement = connection.createStatement();
             resultSet = statement.executeUpdate(query);
             if (resultSet > 0) {
