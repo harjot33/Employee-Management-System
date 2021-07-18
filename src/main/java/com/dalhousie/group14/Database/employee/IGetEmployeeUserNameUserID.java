37ab9aaa3568734b9dd8fc96d9069f5ef2f87d98
Jainam Rakeshkumar Shah <jn326778@dal.ca>
2021-07-18 16:58:35 +0530

    Create Interface for IGetEmployeeUserNameUserID and set indentation.

---
+++ src/main/java/com/dalhousie/group14/Database/employee/IGetEmployeeUserNameUserID.java
@@ -0,0 +1,8 @@
+package com.dalhousie.group14.Database.employee;
+
+public interface IGetEmployeeUserNameUserID {
+
+  int getEmployeeUserIDFromUserName(String username);
+
+  String getEmployeeUserIDFromUserName(int EmployeeID);
+}
