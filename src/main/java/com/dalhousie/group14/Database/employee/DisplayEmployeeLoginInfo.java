5d128edf8a51315538fc845fea7d586192c4ed28
Jainam Rakeshkumar Shah <jn326778@dal.ca>
2021-07-18 17:04:33 +0530

    Update class DisplayEmployeeLoginInfo set indentation, add method descriptions.

--- src/main/java/com/dalhousie/group14/Database/employee/DisplayEmployeeLoginInfo.java
+++ src/main/java/com/dalhousie/group14/Database/employee/DisplayEmployeeLoginInfo.java
@@ -7,27 +7,27 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-import static com.dalhousie.group14.Database.utilities.DbConnection.connectDB;
-
-
 public class DisplayEmployeeLoginInfo {
 
-    public static void  displayEmployeeLoginInfo(String username){
-        Statement statement;
-        ResultSet resultSet;
-        String query= "select * from LoginInfo where UserName ='"+username+"'";
-        try {
-            Connection connection= DbConnection.connectDB();
-            statement=connection.createStatement();
-            resultSet=statement.executeQuery(query);
-            while (resultSet.next()){
-                System.out.println("EmployeeUserName" + resultSet.getString("UserName"));
-                System.out.println("Password" + resultSet.getString("Password"));
-                System.out.println("userType"+ resultSet.getString("userType"));
-            }
+  /* This method Display Employee information.
+   */
+  public void displayEmployeeLoginInfo(String username) {
 
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
+    Statement statement;
+    ResultSet resultSet;
+    String query = "select * from LoginInfo where UserName ='" + username + "'";
+    try {
+      Connection connection = DbConnection.connectDB();
+      statement = connection.createStatement();
+      resultSet = statement.executeQuery(query);
+      while (resultSet.next()) {
+        System.out.println("EmployeeUserName" + resultSet.getString("UserName"));
+        System.out.println("Password" + resultSet.getString("Password"));
+        System.out.println("userType" + resultSet.getString("userType"));
+      }
+
+    } catch (SQLException throwables) {
+      throwables.printStackTrace();
     }
+  }
 }
