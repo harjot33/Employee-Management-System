503fa49219e9acf2bd6940df6eba6babc9b83c06
Jainam Rakeshkumar Shah <jn326778@dal.ca>
2021-07-18 16:58:06 +0530

    Create Interface for IFindDistanceBettweenDates and set indentation.

---
+++ src/main/java/com/dalhousie/group14/BusinessLogic/employee/IFindDistanceBettweenDates.java
@@ -0,0 +1,6 @@
+package com.dalhousie.group14.BusinessLogic.employee;
+
+public interface IFindDistanceBettweenDates {
+
+  long findDistanceBettweenDates(String startingLeaveDate, String endingLeaveDate);
+}
