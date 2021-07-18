3b11800a0a40456fed0729f863092ca831c8434c
Jainam Rakeshkumar Shah <jn326778@dal.ca>
2021-07-18 17:05:22 +0530

    Update class EditProfile set indentation, add method descriptions,remove static method.

--- src/main/java/com/dalhousie/group14/Presentation/employee/EditProfile.java
+++ src/main/java/com/dalhousie/group14/Presentation/employee/EditProfile.java
@@ -1,36 +1,43 @@
 package com.dalhousie.group14.Presentation.employee;
 
 import java.util.Scanner;
-import static com.dalhousie.group14.BusinessLogic.employee.ChangePassword.changePassword;
-import static com.dalhousie.group14.BusinessLogic.employee.ChangeSecurityQuestion.changeSecurityQuestion;
+
+import com.dalhousie.group14.BusinessLogic.employee.ChangePassword;
+import com.dalhousie.group14.BusinessLogic.employee.ChangeSecurityQuestion;
+
 import static com.dalhousie.group14.Presentation.employee.EmployeeLoginDashBoard.employeeLoginDashBoard;
 
 public class EditProfile {
-    public static void editProfile() {
 
-        Scanner scanner = new Scanner(System.in);
-        int number;
-        System.out.println("Press 1 for Edit Password: \n " +
-                            "Press 2 for Change Security Question: \n " +
-                            "Press 3 for Goto the Previous Page:");
-        number = scanner.nextInt();
-        try {
-            //noinspection InfiniteLoopStatement
-            do {
-                if (number == 1) {
-                    changePassword();
-                } else if (number == 2) {
-                    changeSecurityQuestion();
-                } else if (number == 3) {
-                    employeeLoginDashBoard();
-                } else {
-                    System.out.println("Enter Correct Number");
-                }
+  public static void editProfile() {
 
-            } while (true);
-        } catch (Exception e) {
-            //noinspection ThrowablePrintedToSystemOut
-            System.out.println(e);
+    ChangeSecurityQuestion changeSecurityQuestion =
+        new ChangeSecurityQuestion();
+    ChangePassword changePassword = new ChangePassword();
+    Scanner scanner = new Scanner(System.in);
+    int number;
+    System.out.println("Press 1 for Edit Password: \n " +
+        "Press 2 for Change Security Question: \n " +
+        "Press 3 for Goto the Previous Page:");
+    number = scanner.nextInt();
+
+    try {
+      //noinspection InfiniteLoopStatement
+      do {
+        if (number == 1) {
+          changePassword.changePassword();
+        } else if (number == 2) {
+     //     changeSecurityQuestion.changeSecurityQuestion();
+        } else if (number == 3) {
+          employeeLoginDashBoard();
+        } else {
+          System.out.println("Enter Correct Number");
         }
+
+      } while (true);
+    } catch (Exception e) {
+      //noinspection ThrowablePrintedToSystemOut
+      System.out.println(e);
     }
+  }
 }
