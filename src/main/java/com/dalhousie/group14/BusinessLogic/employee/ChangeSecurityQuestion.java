8876fd1bada112fddb3ca84d0130b1bc1a3aa47f
Jainam Rakeshkumar Shah <jn326778@dal.ca>
2021-07-18 17:04:02 +0530

    Update class ChangeSecurityQuestion set indentation, add method descriptions.

--- src/main/java/com/dalhousie/group14/BusinessLogic/employee/ChangeSecurityQuestion.java
+++ src/main/java/com/dalhousie/group14/BusinessLogic/employee/ChangeSecurityQuestion.java
@@ -1,39 +1,42 @@
 package com.dalhousie.group14.BusinessLogic.employee;
 
+import com.dalhousie.group14.Database.employee.DBSecurityQuestion;
+
 import java.util.Scanner;
 
-import static com.dalhousie.group14.Database.employee.UpdateSecurityAnswer.updateSecurityAnswer;
-import static com.dalhousie.group14.Database.employee.UpdateSecurityQuestion.updateSecurityQuestion;
+public class ChangeSecurityQuestion implements IChangeSecurityQuestion {
 
-public class ChangeSecurityQuestion {
+  @Override
+  public void changeSecurityQuestion() {
 
-    public static void changeSecurityQuestion() {
-        String username;
-        String question1, answer1;
-        String question2, answer2;
-        String question3, answer3;
+    DBSecurityQuestion dbSecurityQuestion = new DBSecurityQuestion();
+    String username;
+    String question1, answer1;
+    String question2, answer2;
+    String question3, answer3;
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter UserName:");
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter UserName:");
 
-        username = scanner.nextLine();
-        System.out.println("Enter Security Question 1:");
-        question1 = scanner.nextLine();
-        System.out.println("Enter Answer for Security Question 1:");
-        answer1 = scanner.nextLine();
+    username = scanner.nextLine();
+    System.out.println("Enter Security Question 1:");
+    question1 = scanner.nextLine();
+    System.out.println("Enter Answer for Security Question 1:");
+    answer1 = scanner.nextLine();
 
-        System.out.println("Enter Security Question 2:");
-        question2 = scanner.nextLine();
-        System.out.println("Enter Answer for Security Question 2:");
-        answer2 = scanner.nextLine();
+    System.out.println("Enter Security Question 2:");
+    question2 = scanner.nextLine();
+    System.out.println("Enter Answer for Security Question 2:");
+    answer2 = scanner.nextLine();
 
-        System.out.println("Enter Security Question 3:");
-        question3 = scanner.nextLine();
-        System.out.println("Enter Answer for Security Question 3:");
-        answer3 = scanner.nextLine();
+    System.out.println("Enter Security Question 3:");
+    question3 = scanner.nextLine();
+    System.out.println("Enter Answer for Security Question 3:");
+    answer3 = scanner.nextLine();
 
-        updateSecurityQuestion(username, question1, question2, question3);
-        updateSecurityAnswer(username, answer1, answer2, answer3);
+    dbSecurityQuestion.updateSecurityQuestion(username, question1, question2, question3);
+    dbSecurityQuestion.updateSecurityAnswer(username, answer1, answer2, answer3);
 
-    }
+
+  }
 }
