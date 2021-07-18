56ded4997eaaa8acfe84b856fb0b986aac81d770
Jainam Rakeshkumar Shah <jn326778@dal.ca>
2021-07-18 16:45:00 +0530

    SecurityQuestion.java update with indentation and formatting.

--- src/main/java/com/dalhousie/group14/Presentation/employee/SecurityQuestion.java
+++ src/main/java/com/dalhousie/group14/Presentation/employee/SecurityQuestion.java
@@ -1,34 +1,40 @@
 package com.dalhousie.group14.Presentation.employee;
 
+import com.dalhousie.group14.BusinessLogic.employee.GiveSecurityQuestionAnswer;
+import com.dalhousie.group14.BusinessLogic.employee.SetSecurityQuestion;
+
 import java.util.Scanner;
 
-import static com.dalhousie.group14.BusinessLogic.employee.GiveSecurityQuestionAnswer.giveSecurityQuestionAnswer;
-import static com.dalhousie.group14.BusinessLogic.employee.SetSecurityQuestion.setSecurityQuestion;
-
 public class SecurityQuestion {
-    public static void securityQuestion(String userid) {
-        String username;
-        username = userid;
-        Scanner scanner = new Scanner(System.in);
-        int select;
 
-        System.out.println("Your UserName is:" + username);
-        System.out.println("Press 1 for create Security Question Answer (For First time User): \n" +
-                            "Press 2 for give the answer of security question.");
-        try {
-            //noinspection InfiniteLoopStatement
-            do {
-                select = scanner.nextInt();
-                if (select == 1) {
-                    setSecurityQuestion(username);
-                } else if (select == 2) {
-                    giveSecurityQuestionAnswer(username);
-                } else {
-                    System.out.println("Please Enter Right Choice:");
-                }
-            } while (true);
-        } catch (Exception e) {
-            System.out.println("Exception:"+e);
+  public void securityQuestion(String userid) {
+
+    String username;
+    username = userid;
+    Scanner scanner = new Scanner(System.in);
+    int select;
+    SetSecurityQuestion securityQuestion = new SetSecurityQuestion();
+    GiveSecurityQuestionAnswer giveSecurityQuestionAnswer =
+        new GiveSecurityQuestionAnswer();
+
+    System.out.println("Your UserName is:" + username);
+    System.out.println("Press 1 for create Security Question Answer (For First time User): \n" +
+        "Press 2 for give the answer of security question.");
+
+    try {
+      //noinspection InfiniteLoopStatement
+      do {
+        select = scanner.nextInt();
+        if (select == 1) {
+          securityQuestion.setSecurityQuestion(username);
+        } else if (select == 2) {
+          giveSecurityQuestionAnswer.giveSecurityQuestionAnswer(username);
+        } else {
+          System.out.println("Please Enter Right Choice:");
         }
+      } while (true);
+    } catch (Exception e) {
+      System.out.println("Exception:" + e);
     }
+  }
 }
