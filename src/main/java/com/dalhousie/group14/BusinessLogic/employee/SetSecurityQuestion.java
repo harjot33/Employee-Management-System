b5b62d3bbb0b7e1f2cf11a2944a3d3eb141b5242
Jainam Rakeshkumar Shah <jn326778@dal.ca>
2021-07-18 16:46:19 +0530

    SetSecurityQuestion.java update with indentation and formatting, implement interface, add method description.

--- src/main/java/com/dalhousie/group14/BusinessLogic/employee/SetSecurityQuestion.java
+++ src/main/java/com/dalhousie/group14/BusinessLogic/employee/SetSecurityQuestion.java
@@ -1,43 +1,67 @@
 package com.dalhousie.group14.BusinessLogic.employee;
 
+import com.dalhousie.group14.Database.employee.DBSecurityQuestion;
+
 import java.util.ArrayList;
 import java.util.Scanner;
-import com.dalhousie.group14.Database.utilities.DbConnection;
-import static com.dalhousie.group14.Database.employee.GetSecurityQuestion.getSecurityQuestion;
-import static com.dalhousie.group14.Database.employee.InsertSecurityAnswer.insertSecurityAnswer;
-import static com.dalhousie.group14.Database.employee.SetSecurityQuestionFirstTime.setSecurityQuestionFirstTime;
 
+/* Author- Jainam Shah(B00883898)
+ * SetSecurityQuestion java class contains three task. One is to ask Security
+ * from employee and insert data into database. Second is to ask the answers
+ * of this question. And 3rd one is to insert Security Question and Answer in
+ *  database.
+ */
+public class SetSecurityQuestion implements ISetSecurityQuestion {
 
-public class SetSecurityQuestion {
+  /**
+   * This method take userName as as parameter and insert Security Question
+   * Answer into database.
+   *
+   * @param userName
+   */
+  @Override
+  public void setSecurityQuestion(String userName) {
 
-    public static void setSecurityQuestion(String username) {
-        String question1, question2,
-                question3, answer1, answer2, answer3;
+    String question1;
+    String question2;
+    String question3;
+    String answer1;
+    String answer2;
+    String answer3;
 
-        String userid = username;
-        ArrayList<String> arrayList;
-        Scanner scanner = new Scanner(System.in);
+    /*Create Object of DBSecurityQuestion*/
+    DBSecurityQuestion dbSecurityQuestion = new DBSecurityQuestion();
 
-        setSecurityQuestionFirstTime(userid);
-        arrayList = getSecurityQuestion(userid);
+    Scanner scanner = new Scanner(System.in);
+    String userId = userName;
+    ArrayList<String> arrayList;
 
-        userid = arrayList.get(0);
-        System.out.println("Username:" + userid);
-        System.out.println("Please Write the answer of questions:");
+    /* take userName and insert Security Question in the with userName and
+    questions in the database.
+     */
+    dbSecurityQuestion.setSecurityQuestionFirstTime(userId);
 
-        question1 = arrayList.get(1);
-        System.out.println(question1);
-        answer1 = scanner.nextLine();
+    /*All security Question Answer stored in arrayList ans we can access it.*/
+    arrayList = dbSecurityQuestion.getSecurityQuestion(userId);
 
-        question2 = arrayList.get(2);
-        System.out.println(question2);
-        answer2 = scanner.nextLine();
+    userId = arrayList.get(0);
+    System.out.println("Username:" + userId);
+    System.out.println("Please Write the answer of questions:");
 
-        question3 = arrayList.get(3);
-        System.out.println(question3);
-        answer3 = scanner.nextLine();
+    question1 = arrayList.get(1);
+    System.out.println(question1);
+    answer1 = scanner.nextLine();
 
-        insertSecurityAnswer(userid, answer1, answer2, answer3);
+    question2 = arrayList.get(2);
+    System.out.println(question2);
+    answer2 = scanner.nextLine();
 
-    }
+    question3 = arrayList.get(3);
+    System.out.println(question3);
+    answer3 = scanner.nextLine();
+
+    /*Insert security Question Answer into database.*/
+    dbSecurityQuestion.insertSecurityAnswer(userId, answer1, answer2,
+        answer3);
+  }
 }
