857fa92f30405d180dc83e9407521d11b5a36bef
Jainam Rakeshkumar Shah <jn326778@dal.ca>
2021-07-18 17:06:13 +0530

    Update class UpdatePassword set indentation, add method descriptions,remove static method.

--- src/main/java/com/dalhousie/group14/Database/employee/UpdatePassword.java
+++ src/main/java/com/dalhousie/group14/Database/employee/UpdatePassword.java
@@ -6,26 +6,29 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-
 public class UpdatePassword {
-    public static void updatePassword(String username, String password) {
-        Statement statement;
-        int resultSet;
 
-        String query = "UPDATE `ems`.`LoginInfo` SET `Password` = '" + password + "' WHERE (`UserName` = '" + username + "')";
-        try {
-            Connection connection= DbConnection.connectDB();
-            statement = connection.createStatement();
-            resultSet = statement.executeUpdate(query);
-            if (resultSet > 0) {
-                System.out.println("Update Successfully");
-            } else {
-                System.out.println("Password not update please check your employee id.");
-            }
+  /* This method Update the password in the database.
+   */
+  public void updatePassword(String username, String password) {
 
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
+    Statement statement;
+    int resultSet;
 
+    String query = "UPDATE `ems`.`LoginInfo` SET `Password` = '" + password + "' WHERE (`UserName` = '" + username + "')";
+    try {
+      Connection connection = DbConnection.connectDB();
+      statement = connection.createStatement();
+      resultSet = statement.executeUpdate(query);
+      if (resultSet > 0) {
+        System.out.println("Update Successfully");
+      } else {
+        System.out.println("Password not update please check your employee id.");
+      }
+
+    } catch (SQLException throwables) {
+      throwables.printStackTrace();
     }
+
+  }
 }
