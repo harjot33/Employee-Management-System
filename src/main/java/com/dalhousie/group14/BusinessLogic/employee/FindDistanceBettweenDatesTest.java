5506b8d19f517d8519fdc310c135a3b633c7dd9c
Jainam Rakeshkumar Shah <jn326778@dal.ca>
2021-07-19 15:28:07 +0530

    Create class indDistanceBettweenDatesTest.

---
+++ src/main/java/com/dalhousie/group14/BusinessLogic/employee/FindDistanceBettweenDatesTest.java
@@ -0,0 +1,17 @@
+package com.dalhousie.group14.BusinessLogic.employee;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FindDistanceBettweenDatesTest {
+
+  @Test
+  void findDistanceBettweenDates() {
+    FindDistanceBettweenDates findDistanceBettweenDates=
+        new FindDistanceBettweenDates();
+    assertEquals(3,findDistanceBettweenDates.findDistanceBettweenDates("2021" +
+        "-12-12","2021-12" +
+        "-15"));
+  }
+}
