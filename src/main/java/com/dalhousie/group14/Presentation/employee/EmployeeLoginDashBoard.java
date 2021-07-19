58b33d016eb922e947d0c94d96662a4d59a05ec5
Jainam Rakeshkumar Shah <jn326778@dal.ca>
2021-07-19 15:33:05 +0530

    Update class EmployeeLoginDashBoard.

--- src/main/java/com/dalhousie/group14/Presentation/employee/EmployeeLoginDashBoard.java
+++ src/main/java/com/dalhousie/group14/Presentation/employee/EmployeeLoginDashBoard.java
@@ -1,7 +1,16 @@
 package com.dalhousie.group14.Presentation.employee;
 
+/*This page for Employee login dashboard where employee has options to
+edit profile
+request for leave
+request for medicliam
+request for asset
+request for resign.
+*/
+
 public class EmployeeLoginDashBoard {
     public static void employeeLoginDashBoard(){
-        System.out.println("Welcome to Login Dashbooard");
+        System.out.println("Welcome to Login Dash booard");
+
     }
 }
