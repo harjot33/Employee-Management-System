bc3a009a363a0c7a57431e35cfa7a42cfe13eb3a
Aadil Sadik Mohmed Shaikh <ad979991@dal.ca>
2021-07-20 12:40:35 +0530

    Small fix for notification class. Moved the printing code to presentation layer

---
+++ Tests/com/dalhousie/group14/BusinessLogic/utilities/NotificationManagerTest.java
@@ -0,0 +1,16 @@
+package com.dalhousie.group14.BusinessLogic.utilities;
+
+import org.junit.Test;
+
+import java.sql.SQLException;
+
+import static org.junit.Assert.*;
+
+public class NotificationManagerTest {
+
+    @Test
+    public void getNotifications() throws SQLException {
+        NotificationManager nm = new NotificationManager();
+        nm.getNotifications("891000");
+    }
+}
