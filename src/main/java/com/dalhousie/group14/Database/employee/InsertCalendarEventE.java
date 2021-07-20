c1a4625e195907464cd29b784a9478a7b8bf3710
Aadil Sadik Mohmed Shaikh <ad979991@dal.ca>
2021-07-21 01:08:02 +0530

    Setup event class for calendar.

---
+++ src/main/java/com/dalhousie/group14/Database/employee/InsertCalendarEventE.java
@@ -0,0 +1,12 @@
+package com.dalhousie.group14.Database.employee;
+
+import com.dalhousie.group14.BusinessLogic.employee.CalendarEvent;
+import com.dalhousie.group14.Database.utilities.QueryExecutor;
+
+import java.sql.ResultSet;
+
+public class InsertCalendarEventE {
+    public void insertEvent(CalendarEvent calendarEvent){
+        // ADD LOGIC
+    }
+}
