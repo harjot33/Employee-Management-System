8b5e4b5a70dbf011d08b0e80f2584da119143d77
Jainam Rakeshkumar Shah <jn326778@dal.ca>
2021-07-21 21:07:21 +0530

    create EmployeeLoginDashBoard and use switchcase.

--- src/main/java/com/dalhousie/group14/Presentation/employee/EmployeeLoginDashBoard.java
+++ src/main/java/com/dalhousie/group14/Presentation/employee/EmployeeLoginDashBoard.java
@@ -8,9 +8,37 @@ request for asset
 request for resign.
 */
 
-public class EmployeeLoginDashBoard {
-    public static void employeeLoginDashBoard(){
-        System.out.println("Welcome to Login Dash booard");
+import com.dalhousie.group14.BusinessLogic.employee.RequestForLeave;
 
+import java.util.Scanner;
+
+public class EmployeeLoginDashBoard {
+
+  public void employeeLoginDashBoard() {
+
+    EditProfile editProfile = new EditProfile();
+    RequestForLeave request = new RequestForLeave();
+    System.out.println("Welcome to Login Dashboard");
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Select any of the following task to perform.");
+    System.out.println("Select 1 for Edit Profile:\nSelect 2 for " +
+        "Calender\nSelect 3 for Joining Request:\nSelect 4 for Leave " +
+        "Request:\nSelect 5 for Mediclaim Request:\nSelect 6 for Asset Request:\nSelect 7 for Resign Request:");
+
+    int select;
+    select = scanner.nextInt();
+    switch (select) {
+      case 1:
+        editProfile.editProfile();
+        break;
+      case 2:
+        System.out.println("See Calender:");
+        break;
+      case 3:
+        System.out.println("Joining Request");
+        break;
+      case 4:
+        request.requestForLeave();
     }
+  }
 }
