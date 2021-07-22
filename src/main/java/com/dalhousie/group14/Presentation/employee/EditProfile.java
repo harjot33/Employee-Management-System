905ba135b8aef8b28dd838567e70c81f78065031
Jainam Rakeshkumar Shah <jn326778@dal.ca>
2021-07-22 21:31:18 +0530

    Update Exception Handling in EditEmployeeInformation.

--- src/main/java/com/dalhousie/group14/Presentation/employee/EditProfile.java
+++ src/main/java/com/dalhousie/group14/Presentation/employee/EditProfile.java
@@ -4,7 +4,8 @@ import java.util.Scanner;
 
 import com.dalhousie.group14.BusinessLogic.employee.ChangePassword;
 import com.dalhousie.group14.BusinessLogic.employee.ChangeSecurityQuestion;
-
+/*Author Name-Jainam Shah
+ */
 public class EditProfile {
 
   public void editProfile() {
@@ -17,29 +18,31 @@ public class EditProfile {
     ChangePassword changePassword = new ChangePassword();
     Scanner scanner = new Scanner(System.in);
     int number;
-    System.out.println("Press 1 for Edit Password: \n " +
-        "Press 2 for Change Security Question: \n " +
-        "Press 3 for Goto the Previous Page:");
-    number = scanner.nextInt();
-
-    try {
-      switch (number) {
-        case 1:
-          changePassword.changePassword();
-          break;
-        case 2:
-          changeSecurityQuestion.changeSecurityQuestion();
-          break;
-        case 3:
-          employeeLoginDashBoard.employeeLoginDashBoard();
-          break;
-        default:
-          System.out.println("enter valid number:");
-          break;
+    boolean decision = true;
+    do {
+      try {
+        System.out.println("Press 1 for Edit Password: \n" +
+            "Press 2 for Change Security Question: \n" +
+            "Press 3 for Goto the Previous Page:\nPress 4 to exit this page.");
+        number = scanner.nextInt();
+        switch (number) {
+          case 1:
+            changePassword.changePassword();
+            break;
+          case 2:
+            changeSecurityQuestion.changeSecurityQuestion();
+            break;
+          case 3:
+            employeeLoginDashBoard.employeeLoginDashBoard();
+            break;
+          case 4:
+            decision=false;
+          default:
+            System.out.println("enter valid number:");
+        }
+      } catch (Exception e) {
+        System.out.println("Exception in EditProfile:" + e);
       }
-
-    } catch (Exception e) {
-      System.out.println(e);
-    }
+    }while (decision);
   }
 }
