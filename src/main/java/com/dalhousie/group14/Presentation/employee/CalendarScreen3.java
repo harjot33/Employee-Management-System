d6c92b9551d9e88b728ae31e3efad9a96e1a07a9
Aadil Sadik Mohmed Shaikh <ad979991@dal.ca>
2021-07-22 16:52:48 +0530

    Bug fix calendar modify event

--- src/main/java/com/dalhousie/group14/Presentation/employee/CalendarScreen3.java
+++ src/main/java/com/dalhousie/group14/Presentation/employee/CalendarScreen3.java
@@ -3,13 +3,22 @@ package com.dalhousie.group14.Presentation.employee;
 import com.dalhousie.group14.BusinessLogic.employee.CalendarEvent;
 import com.dalhousie.group14.Database.employee.getEvent;
 import com.dalhousie.group14.Presentation.Common.UserInput;
+import com.dalhousie.group14.Presentation.utilities.CalendarDisplay;
 
 import java.sql.SQLException;
+import java.time.LocalDate;
 
 public class CalendarScreen3 implements CalendarScreen{
     public void displayScreen(){
-        System.out.println("Enter the name of event: ");
-        String name = UserInput.takeString();
+        LocalDate currentDate = LocalDate.now();
+        int currentYear = currentDate.getYear();
+        int currentMonth = currentDate.getMonth().getValue();
+        CalendarDisplay calendarDisplay = new CalendarDisplay();
+        try {
+            calendarDisplay.displayThreeMonths(currentYear,currentMonth);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
         System.out.println("Enter the date of event (FORMAT - YYYY-MM-DD): ");
         System.out.println("Please follow the format (YYYY-DD-MM) EG [2021-12-08]");
         String date = UserInput.takeString();
@@ -26,10 +35,13 @@ public class CalendarScreen3 implements CalendarScreen{
                 String newdesc = UserInput.takeString();
                 CalendarEvent enew = new CalendarEvent(newname,newdesc,newdate);
                 getEvent.searchEventAndModify(enew,date);
+                System.out.println("Updated the event successfully");
             }
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
+        CalendarScreen1 cs1 = new CalendarScreen1();
+        cs1.displayScreen();
 
     }
 }
