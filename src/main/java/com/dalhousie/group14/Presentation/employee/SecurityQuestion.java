3cf0b772a7e2cf7a7fb7512ff7a5bde30fd4345d
Jainam Rakeshkumar Shah <jn326778@dal.ca>
2021-07-22 13:11:36 +0530

    Update Exception Handling in Security Question class.

--- src/main/java/com/dalhousie/group14/Presentation/employee/SecurityQuestion.java
+++ src/main/java/com/dalhousie/group14/Presentation/employee/SecurityQuestion.java
@@ -7,34 +7,45 @@ import java.util.Scanner;
 
 public class SecurityQuestion {
 
-  public void securityQuestion(String userid) {
+  public void securityQuestion() {
 
-    String username;
-    username = userid;
-    Scanner scanner = new Scanner(System.in);
-    int select;
     SetSecurityQuestion securityQuestion = new SetSecurityQuestion();
     GiveSecurityQuestionAnswer giveSecurityQuestionAnswer =
         new GiveSecurityQuestionAnswer();
-
-    System.out.println("Your UserName is:" + username);
+    Scanner scanner = new Scanner(System.in);
+    Scanner scanner1 = new Scanner(System.in);
+    String userName;
+    System.out.println("Enter your UserName:");
+    userName = scanner1.nextLine();
+    System.out.println("Your UserName is:" + userName);
     System.out.println("Press 1 for create Security Question Answer (For First time User): \n" +
         "Press 2 for give the answer of security question.");
 
+    int select;
+    int counter = 0;
     try {
-      //noinspection InfiniteLoopStatement
       do {
         select = scanner.nextInt();
-        if (select == 1) {
-          securityQuestion.setSecurityQuestion(username);
-        } else if (select == 2) {
-          giveSecurityQuestionAnswer.giveSecurityQuestionAnswer(username);
-        } else {
-          System.out.println("Please Enter Right Choice:");
+        switch (select) {
+          case 1:
+            securityQuestion.setSecurityQuestion(userName);
+            break;
+          case 2:
+            giveSecurityQuestionAnswer.giveSecurityQuestionAnswer(userName);
+            break;
+          default:
+            System.out.println("Enter Only 1 and 2 as per your Requirements.");
+            counter = counter + 1;
+        }
+        if (counter == 3) {
+          System.out.println("Sorry, EMS System not allow to enter wrong choice more then 3 times.");
+          break;
         }
       } while (true);
+    } catch (NumberFormatException formatException) {
+      System.out.println("NumberFormat Exception:" + formatException);
     } catch (Exception e) {
-      System.out.println("Exception:" + e);
+      e.printStackTrace();
     }
   }
 }
