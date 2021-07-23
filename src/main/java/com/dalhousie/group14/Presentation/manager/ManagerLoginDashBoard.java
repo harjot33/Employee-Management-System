643256413f538c4de181e7dfaafadd661a13b5ba
Jainam Rakeshkumar Shah <jn326778@dal.ca>
2021-07-23 20:46:11 +0530

    Update ManagerLoginDashBoard  .

--- src/main/java/com/dalhousie/group14/Presentation/manager/ManagerLoginDashBoard.java
+++ src/main/java/com/dalhousie/group14/Presentation/manager/ManagerLoginDashBoard.java
@@ -9,10 +9,10 @@ import java.util.Scanner;
 public class ManagerLoginDashBoard {
 
   public void managerLoginDashBoard() {
+
     Scanner scanner = new Scanner(System.in);
-
     ManageEmployee ManageEmployee = new ManageEmployee();
-
+    ProjectManagement projectManagement=new ProjectManagement();
     IRequestForApprovals request = new RequestForApprovals();
 
     System.out.println("\t\t\t\t\t\t\t\t\t\t\t\tWelcome to Manager Dashboard\t\t\t\t\t\t\t\t\t\t\t\t");
@@ -21,12 +21,14 @@ public class ManagerLoginDashBoard {
 
 
     int select;
-    boolean decision=true;
+    boolean decision = true;
     do {
       try {
-        System.out.println("Select 1 for ManageEmployee:\nSelect 2 for Approve " +
-            "Requests:\nSelect 3 for Project Management:\nSelect 4 for " +
-                "LogOut:\nSelect 5 for Exit.");
+        System.out.println("Enter 1 for ManageEmployee:\nEnter 2 for Approve Requests:" +
+            "\nEnter 3 for Project Management:\nEnter 4 for Performance " +
+            "Evaluation:\nEnter 5 for Notifications:\nEnter 6 for Employee " +
+            "Schedule Management:\nEnter 7 for Employee Salary " +
+            "Management\nEnter 8 for Logout\nEnter 9 for exit from this page.");
         select = scanner.nextInt();
         switch (select) {
           case 1:
@@ -38,19 +40,33 @@ public class ManagerLoginDashBoard {
             System.out.println("Requests");
             break;
           case 3:
+            projectManagement.projectManagement();
             System.out.println("Project Management");
             break;
           case 4:
-            System.out.println("Logout");
+            System.out.println("Performance Evaluation");
             break;
           case 5:
-            decision=false;
+            System.out.println("Notifications");
+            break;
+          case 6:
+            System.out.println("Employee Schedule Management");
+            break;
+          case 7:
+            System.out.println("Employee Salary Management");
+            break;
+          case 8:
+            System.out.println("Logout");
+            break;
+          case 9:
+            decision = false;
+            break;
           default:
             System.out.println("Enter Correct number:");
         }
       } catch (Exception e) {
-        System.out.println("Exception in ManagerLoginDashboard"+e);
+        System.out.println("Exception in ManagerLoginDashboard" + e);
       }
-    }while (decision);
+    } while (decision);
   }
 }
