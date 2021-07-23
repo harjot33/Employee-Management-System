61f657b2d23104b3bdee3611f685e7ce25bc23c3
Jainam Rakeshkumar Shah <jn326778@dal.ca>
2021-07-23 20:48:55 +0530

    Create interface IGetEmployeeUserNameUserID  .

--- src/main/java/com/dalhousie/group14/Database/employee/IGetEmployeeUserNameUserID.java
+++ src/main/java/com/dalhousie/group14/Database/employee/IGetEmployeeUserNameUserID.java
@@ -1,8 +1,13 @@
 package com.dalhousie.group14.Database.employee;
 
+/*Author- Jainam Shah(B00883898)*/
 public interface IGetEmployeeUserNameUserID {
 
-  int getEmployeeUserIDFromUserName(String username);
+  /* Take userName as a parameter and return the UserID.
+   */
+  int getEmployeeUserIDFromUserName(String userName);
 
-  String getEmployeeUserIDFromUserName(int EmployeeID);
+  /* Take userID as a parameter and return the UserName.
+   */
+  String getEmployeeUserNameFromUserID(int employeeID);
 }
