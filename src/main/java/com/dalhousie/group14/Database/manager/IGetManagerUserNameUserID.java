f764dd1b6805545099221dca063eb76131d3410a
Jainam Rakeshkumar Shah <jn326778@dal.ca>
2021-07-23 20:45:50 +0530

    Write author name and remove override .

--- src/main/java/com/dalhousie/group14/Database/manager/IGetManagerUserNameUserID.java
+++ src/main/java/com/dalhousie/group14/Database/manager/IGetManagerUserNameUserID.java
@@ -1,5 +1,6 @@
 package com.dalhousie.group14.Database.manager;
 
+/*Author- Jainam Shah(B00883898)*/
 public interface IGetManagerUserNameUserID {
 
   int getManagerUserIDFromUserName(String managerName);
