c743a5a383b3b79e2c16aa3c2d7cd11388e4ae22
Jainam Rakeshkumar Shah <jn326778@dal.ca>
2021-07-27 13:18:00 +0530

    Initial commit.

--- src/main/java/com/dalhousie/group14/BusinessLogic/employee/Employee.java
+++ src/main/java/com/dalhousie/group14/BusinessLogic/employee/Employee.java
@@ -1,13 +1,14 @@
 package com.dalhousie.group14.BusinessLogic.employee;
 
 public class Employee {
-    private String userName;
-    public Employee(String userName){
-        this.userName=userName;
-    }
 
+  private String userName;
 
-    public String getUserName() {
-        return this.userName;
-    }
+  public Employee(String userName) {
+    this.userName = userName;
+  }
+
+  public String getUserName() {
+    return this.userName;
+  }
 }
